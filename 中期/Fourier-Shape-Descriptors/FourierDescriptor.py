a8083ef68e9be55f70d30cd0b4bd9dd18f0c6be6
Luo Yuchen <1245709462@qq.com>
2019-05-04 17:59:03 +0800

    Fourier

---
+++ 中期/Fourier-Shape-Descriptors/FourierDescriptor.py
@@ -0,0 +1,97 @@
+# python 2.7
+
+import cv2 as cv
+import numpy as np
+
+def reconstruct_contour(contour, resolution):
+    contour_reconstruct  = contour.copy()
+    
+    # make positive
+    if contour_reconstruct.min() < 0:
+        contour_reconstruct -= contour_reconstruct.min()
+
+    # normalization
+    contour_reconstruct *= resolution / contour_reconstruct.max()
+
+    # type cast to int32
+    contour_reconstruct = contour_reconstruct.astype(np.int32, copy=False)
+    
+    return contour_reconstruct
+
+def findDescriptor(img, resolution):
+    """ findDescriptor(img) finds and returns the
+    Fourier-Descriptor of the image contour"""
+    # convert to gray image
+    imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # threshold
+    ret, thresh = cv.threshold(imgray, 1, 255, 0)
+
+    # detect contours
+    contour, hierarchy = cv.findContours(
+        thresh,
+        cv.RETR_EXTERNAL,
+        cv.CHAIN_APPROX_NONE)
+
+    print contour[0].shape
+    contour_array = contour[0][:, 0, :]
+    # print contour_array.shape
+    # contour_array = reconstruct_contour(contour_array, resolution)
+    # print contour_array.shape
+
+    contour_complex = np.empty(contour_array.shape[:-1], dtype=complex)
+    contour_complex.real = contour_array[:, 0]
+    contour_complex.imag = contour_array[:, 1]
+    fourier_result = np.fft.fft(contour_complex)
+    return fourier_result
+
+
+def truncate_descriptor(descriptors, degree):
+    """this function truncates an unshifted fourier descriptor array
+    and returns one also unshifted"""
+    descriptors = np.fft.fftshift(descriptors)
+    #draw_descriptors(descriptors, 512, "after fftshift")
+    center_index = len(descriptors) / 2
+    descriptors = descriptors[
+        center_index - degree / 2:center_index + degree / 2]
+    #draw_descriptors(descriptors, 512, "after sample")
+    descriptors = np.fft.ifftshift(descriptors)
+    #draw_descriptors(descriptors, 512, "after ifftshift")
+
+    return descriptors
+
+
+def draw_descriptors(descriptors, resolution, name):
+    """ reconstruct(descriptors, degree) attempts to reconstruct the image
+    using the first [degree] descriptors of descriptors"""
+    # truncate the long list of descriptors to certain length
+    
+    contour_reconstruct = np.fft.ifft(descriptors)
+    contour_reconstruct = np.array(
+        [contour_reconstruct.real, contour_reconstruct.imag])
+    contour_reconstruct = np.transpose(contour_reconstruct)
+    contour_reconstruct = np.expand_dims(contour_reconstruct, axis=1)
+
+    # make positive
+    if contour_reconstruct.min() < 0:
+        contour_reconstruct -= contour_reconstruct.min()
+    # normalization
+    contour_reconstruct *= resolution / contour_reconstruct.max()
+    # type cast to int32
+    contour_reconstruct = contour_reconstruct.astype(np.int32, copy=False)
+    black = np.zeros((resolution,resolution), np.uint8)
+
+    cv.drawContours(black, contour_reconstruct, -1, 255, thickness=-1)
+    cv.imwrite(name+".png", black)
+
+
+def getFourierDescriptor(img, degree, resolution, idx=None):
+    init_f = findDescriptor(img, resolution)
+    recons_f = truncate_descriptor(init_f, degree)
+
+    if idx is not None:
+        draw_descriptors(init_f, resolution, "visualization/%d_before truc"%idx)
+        draw_descriptors(recons_f, resolution, "visualization/%d_after truc"%idx)
+       
+    return recons_f
+
---
+++ 中期/Fourier-Shape-Descriptors/main.py
@@ -0,0 +1,112 @@
+# python 2.7
+
+import cv2 as cv
+import numpy as np
+from FourierDescriptor import getFourierDescriptor
+
+MIN_DESCRIPTOR = 100  # surprisingly enough, 2 descriptors are already enough
+TRAINING_SIZE = 100
+RESOLUTION = 512
+MODEL_NUM = 20
+
+def addNoise(descriptors):
+    """this function adds gaussian noise to descriptors
+    descriptors should be a [N,2] np array"""
+    scale = descriptors.max() / 10
+    noise = np.random.normal(0, scale, descriptors.shape[0])
+    noise = noise + 1j * noise
+    descriptors += noise
+
+
+def sample_generater(descriptors):
+    """this function generates training_set, also for testing"""
+    response = np.arange(MODEL_NUM)
+    response = np.tile(response, TRAINING_SIZE / MODEL_NUM)
+    #response = response.astype(np.float32)
+    training_set = np.empty(
+        [TRAINING_SIZE, MIN_DESCRIPTOR], dtype=np.float32)
+    # assign descriptors with noise to our training_set
+    for i in range(TRAINING_SIZE / MODEL_NUM):
+        for j in range(MODEL_NUM):
+            descriptors_sample = descriptors[j].copy()
+            addNoise(descriptors_sample)
+            training_set[i*MODEL_NUM + j] = np.absolute(descriptors_sample)
+        
+    return training_set, response
+
+
+"""SVM START"""
+def SVM(training_set, response, test_set, answer):
+    # Training
+    svm_model = cv.ml.SVM_create()
+    # set up parameters for SVM
+    svm_model.setKernel(cv.ml.SVM_LINEAR)
+    svm_model.setType(cv.ml.SVM_C_SVC)
+    svm_model.setC(2.67)
+    svm_model.setGamma(5.383)
+    svm_model.train(training_set, cv.ml.ROW_SAMPLE, response)
+    # To my surprise SVM training is already perfect with 2 descriptors
+    # answer_SVM = [svm_model.predict(np.float32(s)) for s in test_set]
+    answer_SVM = svm_model.predict(test_set)
+    answer_SVM = np.array(answer_SVM)[1]
+    #print answer_SVM
+    answer = answer.reshape(TRAINING_SIZE,1)
+    #print answer
+    success_rate_SVM = np.count_nonzero(answer_SVM==answer) *1.0/ TRAINING_SIZE
+    
+    return success_rate_SVM 
+"""SVM END"""
+
+"""Minimum distance classifier"""
+def KNN(training_set, response, test_set, answer):
+    knn = cv.ml.KNearest_create()
+    knn.train(training_set, cv.ml.ROW_SAMPLE,response)
+    ret, answer_KNN, neignbours, distance = knn.findNearest(test_set, 3)
+    answer = answer.reshape(TRAINING_SIZE,1)
+
+    success_rate_KNN = np.count_nonzero(answer_KNN==answer) *1.0/ TRAINING_SIZE
+    return success_rate_KNN
+
+"""Minimum distance classifier END"""
+
+
+if __name__ == "__main__":
+    descriptors = []
+    # import images and treat
+    for idx in range(MODEL_NUM):
+        imgname = '../render_output/m%d_0_512x512.png'%idx  #更改为图片所在位置
+        print "processing m%d_0_512x512.png ... "%idx
+        img = cv.imread(imgname)       
+
+        """Mission 1: Calculate fourier descriptor
+        and reconstruct using minimum amout of descriptors"""
+        fourier_result = getFourierDescriptor(img, MIN_DESCRIPTOR, RESOLUTION, idx)
+        np.save('Descriptors/%d_descriptor_%d.npy'%(idx, MIN_DESCRIPTOR), fourier_result)
+        """Mission 1 END"""
+
+        descriptors.append(fourier_result)
+    
+    descriptors = np.array(descriptors)
+
+    """generate training_set"""
+    training_set, response = sample_generater(descriptors)
+    test_set, correct_answer = sample_generater(descriptors)    
+    """generate training_set END"""
+
+    print "Descriptor Number: %d"%MIN_DESCRIPTOR
+    success_rate_SVM = SVM(training_set, response, test_set, correct_answer)
+    print 'For SVM, success rate (0~1) = '+ str(success_rate_SVM)
+
+    success_rate_KNN = KNN(training_set, response, test_set, correct_answer)
+    print 'For KNN, success_rate_KNN (0~1) = ' + str(success_rate_KNN)
+
+# """Bayers classifier"""
+# bayers_model = cv.ml.NormalBayesClassifier()
+# bayers_model.train(training_set, response)
+# retval, answer_bayers = bayers_model.predict(test_set)
+# success_rate_bayers = np.sum(
+#     np.in1d(
+#         correct_answer,
+#         answer_bayers)) / TRAINING_SIZE
+# print("For bayers_model, success_rate_bayers =  ", success_rate_bayers)
+# """Bayers classifier END"""
